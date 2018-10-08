640147945053caa4ad036e26f470035a00bee687
MikeBLambert <mlamb128@gmail.com>
2018-10-08 16:13:32 -0700

    Can't figure out how to access url!

--- test/app.test.js
+++ test/app.test.js
@@ -2,11 +2,12 @@ const request = require("supertest");
 const app = require("../lib/app");
 
 describe("penguins API", () => {
- 
+    
     it('gets all penguins', () => {
+                 
         return  request(app).get('/api/penguins')
             .then(res => {
-                expect(res.body).toEqual(['bernice', 'bernard'])
+                expect(res.body).toEqual(['bernice', 'bernard']);
             });
     });
 });
