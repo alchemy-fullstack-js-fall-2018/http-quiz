bb8de5e0073a008ddd5aba69c645a386d140ba35
Sarah Flynn <sarah.flynn@icloud.com>
2018-10-08 16:22:31 -0700

    in progress

--- test/app.test.js
+++ test/app.test.js
@@ -8,4 +8,42 @@ describe('penguins', () => {
                 expect(res.body).toEqual(['bernice', 'bernard']);
             });     
     });
+    it('returns a full format penguin', () => {
+        return request(app).get('/api/penguin/king?format=full')
+            .then(res => {
+                expect(res.body).toEqual({ 
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                });
+            });   
+    });
+    it('returns a simple format penguin', () => {
+        return request(app).get('/api/penguin/king?format=simple')
+            .then(res => {
+                expect(res.body).toEqual({ 
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                });
+            });   
+    });
+    it('returns a simple format penguin', () => {
+        return request(app).get('/mistake')
+            .then(res => {
+                expect(res.body).toEqual({ 
+                    name: 'bernice'
+                });
+            });   
+    });
+    it('returns a simple format penguin', () => {
+        return request(app).get('/mistake')
+            .then(res => {
+                expect(res.body).toEqual({ 
+                    name: 'bernice'
+                });
+            });   
+    });
 });
+
+
