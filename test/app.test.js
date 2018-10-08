3d6b0fa2e418e628d3ed5ab2720d07ffc989feb1
Claire Flanagan <claireflanagan@gmail.com>
2018-10-08 15:45:17 -0700

    first test

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,11 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+describe('Penguins API', () => {
+    it('gets a penguin', () => {
+        return request(app).get('/api/penguins')
+            .then(res => {
+                expect(res.body).toEqual(['bernice', 'bernard']);
+            });
+    });
 });
