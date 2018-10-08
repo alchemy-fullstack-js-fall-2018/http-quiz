7bac96c768fedb6ee1169fd017310ec46cb5ec3e
KLPainter <KLPainter@gmail.com>
2018-10-08 15:48:53 -0700

    basic tests

--- test/app.test.js
+++ test/app.test.js
@@ -8,9 +8,38 @@ it('gets two penguins', () => {
         });
 });
 
-it('gets two penguins', () => {
-    return request(app).get('/penguins')
+it('gets simple penguin', () => {
+    return request(app).get('/api/penguin/king?format=simple')
         .then(results => {
-            expect(results).toEqual(['bernice', 'bernard']);
+            expect(results).toEqual({ 
+                name: 'bernice' 
+            });
+        });
+});
+
+it('gets full penguin', () => {
+    return request(app).get('/api/penguin/king?format=full')
+        .then(results => {
+            expect(results).toEqual({ 
+                name: 'bernice',
+                description: 'What a penguin!',
+                age: 7
+            });
+        });
+});
+
+it('deletes as described', () => {
+    return request(app).delete('/mistake')
+        .then(results => {
+            expect(results).toEqual({ deleted: true });
+        });
+});
+
+it('gets simple penguin', () => {
+    return request(app).get('/api/penguin/king?format=simple')
+        .then(results => {
+            expect(results).toEqual({ 
+                name: 'bernice' 
+            });
         });
 });
