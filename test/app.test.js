21ff88c262a69eb3d6b2f15bde13a65f8fcc476c
MikeBLambert <mlamb128@gmail.com>
2018-10-08 15:49:24 -0700

    write getAll test

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,12 @@
 const request = require("supertest");
 const app = require("../lib/app");
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+describe("penguins API", () => {
+ 
+    it('gets all penguins', () => {
+        return  request(app).get('/api/penguins')
+            .then(res => {
+                expect(res.body).toEqual(['bernice', 'bernard'])
+            });
+    });
 });
