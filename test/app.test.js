f0959fbf9e1b6e4e2e01c9afb883294793ae40d8
Davidchhing <davidhchhing@gmail.com>
2018-10-08 15:48:18 -0700

    writing test for penguin

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,27 @@ const request = require("supertest");
 const app = require("../lib/app");
 
 describe("Pirates API", () => {
+
+  it('gets penguin', () => {
+    return request(app).post('/api/penguins')
+        .send(['bernice', 'bernard'])
+        .then(createResponse => {   
+            const { id } = JSON.parse(createResponse.text);
+            return request(app).get(`/api/penguins/${id}`);
+        })
+        .then(getResponse => {
+            const pet = JSON.parse(getResponse.text);
+            expect(pet.id).toEqual(expect.any(String));
+            expect(pet.name).toEqual('porky');
+            expect(pet.petType).toEqual('little piglet');
+            expect(true);
+        });
+});
+['bernice', 'bernard']
   it("write a test...", () => {
-    expect(true);
+    
   });
+
+  'bernice', 'bernard']
+  /api/penguins
 });
