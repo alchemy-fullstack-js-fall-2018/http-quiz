26326d6b475f8d949ebf4d667fbfa5dbbaaea659
Jack <ajackofsometrades@gmail.com>
2018-10-08 15:49:39 -0700

    fixed test to include api path and stringify

--- test/app.test.js
+++ test/app.test.js
@@ -4,8 +4,8 @@ const app = require("../lib/app");
 describe("Penguins API", () => {
   
   it("getALL penguins should return array of penguins in JSON", () => {  
-      return request(app).get('/penguins').then(res => {
-        expect(res.text).toEqual(`['bernice', 'bernard']`);
+      return request(app).get('/api/penguins').then(res => {
+        expect(res.text).toEqual(JSON.stringify(['bernice', 'bernard']));
       });
 
 
