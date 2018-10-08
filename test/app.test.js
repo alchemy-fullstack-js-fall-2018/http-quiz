e9ad3fb570da906a1521fa93a75f1c282b16c015
KLPainter <KLPainter@gmail.com>
2018-10-08 16:25:02 -0700

    who knows?

--- test/app.test.js
+++ test/app.test.js
@@ -4,7 +4,7 @@ const app = require('../lib/app');
 it('gets two penguins', () => {
     const expected = ['bernice', 'bernard'];
     return request(app).get('/penguins').then(res => {
-        expect(res.text).toEqual(expected);
+        expect(res.body).toEqual(expected);
     });
 });
 
