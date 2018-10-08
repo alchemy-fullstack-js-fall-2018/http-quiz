990a57f066d6e1e964afd49aab640d576ace123a
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2018-10-08 16:14:34 -0700

    delete and gets pengiuns working full still failing

--- test/app.test.js
+++ test/app.test.js
@@ -8,4 +8,23 @@ describe('Pirates API', () => {
                 expect(res.body).toEqual(['bernice', 'bernard']);
             });
     });
+
+    it('gets a single penguin with format=full', () => {
+        return request(app).get('//api/penguin/king?format=full')
+            .then(res => {
+                const expected = { 
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                };
+                expect(res.body).toEqual(expected);
+            });
+    });
+
+    it('deletes mistake', () => {
+        return request(app).delete('/mistake')
+            .then(res => {
+                expect(res.body).toEqual({ deleted: true });
+            });
+    });
 });
