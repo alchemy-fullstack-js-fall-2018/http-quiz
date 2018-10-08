852e3222116616ff7eb1065c35d523fab7611472
Alex Rankin <alex.c.rankin@gmail.com>
2018-10-08 16:10:18 -0700

    i have no idea why this isn't working

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,12 @@ describe('Penguins API', () => {
     it('should respond with an array of penguins with GET', () => {
         return request(app).get('/api/penguins')
             .then(res => {
+                console.log(res);
                 expect(res.text).toEqual(['bernice', 'bernard']);
             });  
+    });
+
+    it('should return a penguin as an object with props', () => {
+        return request(app).get('')
     });
 });
