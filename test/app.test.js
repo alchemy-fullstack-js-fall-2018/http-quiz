78c644599eac9acc47266d5b4a5a77843e238192
Ryan Luras <ryanluras@gmail.com>
2018-10-08 16:19:36 -0700

    trying to get penquin route happy

--- test/app.test.js
+++ test/app.test.js
@@ -1,8 +1,22 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("Pirates API", () => {
-  it("write a test...", () => {
-    expect(true);
-  });
+describe('Penguins API', () => {
+
+
+    it('responds with hi there when path is /hello', () => {
+        return request(app).get('/hello')
+            .then(res => {
+                expect(res.text).toEqual('hi there');
+            });
+    });
+
+    it('gets penguins', () => {
+        return request(app)
+            .get('/api/penguins')
+            .then(res => {
+                JSON.parse(res.text);
+                expect(res.text).toEqual(['bernice', 'bernard']);
+            });
+    });
 });
