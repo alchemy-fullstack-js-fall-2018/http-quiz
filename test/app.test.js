2904ac896be972fbb11c1b39c2d741c4f487bff1
Claire Flanagan <claireflanagan@gmail.com>
2018-10-08 16:23:11 -0700

    seemingly very little progress made

--- test/app.test.js
+++ test/app.test.js
@@ -2,10 +2,23 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('Penguins API', () => {
-    it('gets a penguin', () => {
-        return request(app).get('/api/penguins')
+
+    it('posts a penguin', () => {
+        return request(app).post('/api/penguins')
+            .send({ name: 'bernice', description: 'What a penguin!', age: 7 })
             .then(res => {
-                expect(res.body).toEqual(['bernice', 'bernard']);
+                const json = res.body;
+                expect(json.name).toEqual('bernice');
+                expect(json.description).toEqual('What a penguin!');
+                expect(json.age).toEqual(7);
             });
     });
+
+
+    // it('gets a penguin', () => {
+    //     return request(app).get('/api/penguins')
+    //         .then(res => {
+    //             expect(res.body).toEqual(['bernice', 'bernard']);
+    //         });
+    // });
 });
