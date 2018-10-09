65c5481d4f394c2cd0d38f0145fed5d5058b99b5
MikeBLambert <mlamb128@gmail.com>
2018-10-08 19:11:16 -0700

    get all passes

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,16 @@
-const request = require("supertest");
-const app = require("../lib/app");
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe("penguins API", () => {
+describe('penguins API', () => {
     
     it('gets all penguins', () => {
                  
-        return  request(app).get('/api/penguins')
+        return  request(app).get('/penguins')
             .then(res => {
-                expect(res.body).toEqual(['bernice', 'bernard']);
+                
+                expect(JSON.parse(res.text)).toEqual(['bernice', 'bernard']);
             });
     });
+
+    
 });
