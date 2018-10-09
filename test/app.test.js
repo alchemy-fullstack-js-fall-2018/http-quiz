e67977103ea6f0d0cd087e9ae97edaac615bf0b8
MikeBLambert <mlamb128@gmail.com>
2018-10-08 19:29:32 -0700

    get full format queries works

--- test/app.test.js
+++ test/app.test.js
@@ -5,10 +5,20 @@ describe('penguins API', () => {
     
     it('gets all penguins', () => {
                  
-        return  request(app).get('/penguins')
+        return request(app).get('/penguins')
             .then(res => {
-                
                 expect(JSON.parse(res.text)).toEqual(['bernice', 'bernard']);
+            });
+    });
+
+    it('gets custom full format queries', () => {
+        return request(app).get('/penguin/king?format=full')
+            .then(res => {
+                expect(JSON.parse(res.text)).toEqual({ 
+                    name: 'bernice',
+                    description: 'What a penguin!',
+                    age: 7
+                });
             });
     });
 
